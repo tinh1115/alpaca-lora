14f05f703d73599736573ffb1d2ae5a2faf4b0f5
Tinh Dang <tinhdang.quang@potentiateglobal.com>
2023-06-29 19:01:44 +1000

    more

--- export_hf_checkpoint.py
+++ export_hf_checkpoint.py
@@ -7,63 +7,14 @@ from transformers import LlamaForCausalLM, LlamaTokenizer  # noqa: F402
 
 
 BASE_MODEL = 'decapoda-research/llama-7b-hf'
-# BASE_MODEL = os.environ.get("BASE_MODEL", None)
-# assert (
-#     BASE_MODEL
-# ), "Please specify a value for BASE_MODEL environment variable, e.g. `export BASE_MODEL=huggyllama/llama-7b`"  # noqa: E501
-### temporary test ###
-try:
-    base_model = LlamaForCausalLM.from_pretrained(
-    "/content/gdrive/MyDrive/llama-7b",
-    load_in_8bit=False,
-    torch_dtype=torch.float16,
-    evice_map="auto"
-    )
 
-    # Print the state_dict
-    print("Base Model State Dict:")
-    for param_tensor in base_model.state_dict():
-        if hasattr(base_model.state_dict()[param_tensor], "size"):
-            print(param_tensor, "\t", base_model.state_dict()[param_tensor].size())
-
-    # Load the finetuned model
-    lora_model = LlamaForCausalLM.from_pretrained(
-                "/content/gdrive/MyDrive/llama-7b",
-                load_in_8bit=True,
-                torch_dtype=torch.float16,
-                device_map="auto",
-            )
-    lora_model = PeftModel.from_pretrained(
-        lora_model,
-        "/content/gdrive/MyDrive/alpaca-lora/lora-alpaca",
-        torch_dtype=torch.float16,
-    )
-
-    # Print the state_dict
-    print("Finetuned Model State Dict:")
-    for param_tensor in lora_model.state_dict():
-        if hasattr(lora_model.state_dict()[param_tensor], "size"):
-            print(param_tensor, "\t", lora_model.state_dict()[param_tensor].size())
-
-    # Load the finetuned weights directly
-    weights = torch.load('/content/gdrive/MyDrive/alpaca-lora/lora-alpaca/adapter_model.bin')
-    print(weights)
-    # Print the weights
-    print("Directly Loaded Weights:")
-    for param_tensor in weights:
-        # check if weights[param_tensor] has attribute size
-        if hasattr(weights[param_tensor], "size"):
-            print(param_tensor, "\t", weights[param_tensor].size())
-except:
-    print("Failed to load model for testing")
-### end test
 tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
 
 base_model = LlamaForCausalLM.from_pretrained(
     BASE_MODEL,
     load_in_8bit=False,
     torch_dtype=torch.float16,
-    device_map={"": "cpu"},
+     device_map="auto",
 )
 
 first_weight = base_model.model.layers[0].self_attn.q_proj.weight
@@ -72,7 +23,7 @@ first_weight_old = first_weight.clone()
 lora_model = PeftModel.from_pretrained(
     base_model,
     "/content/gdrive/MyDrive/alpaca-lora/lora-alpaca",
-    device_map={"": "cpu"},
+    device_map="auto",
     torch_dtype=torch.float16,
 )
 
